c41db4ae031c963feaed1312d4b85cf5df3a11ff
pattamaporn.k <142088917+pattxmak@users.noreply.github.com>
2025-10-21 20:37:43 +0700

    build (backend): initial and setup backend

---
+++ backend/users-management-system/src/test/java/com/pattama/users_management_system/UsersManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pattama.users_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
