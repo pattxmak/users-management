56b480d24c3e0825bb18315fba34a4d59de1e1c6
pattamaporn.k <142088917+pattxmak@users.noreply.github.com>
2025-10-21 20:39:23 +0700

    feat (entity): add Entity

---
+++ backend/users-management-system/src/main/java/com/pattama/users_management_system/entity/BaseEntity.java
@@ -0,0 +1,17 @@
+package com.pattama.users_management_system.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+
+@Data
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
---
+++ backend/users-management-system/src/main/java/com/pattama/users_management_system/entity/User.java
@@ -0,0 +1,63 @@
+package com.pattama.users_management_system.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Entity
+@Table(name = "ourusers")
+public class User extends BaseEntity implements UserDetails {
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+    private String city;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
